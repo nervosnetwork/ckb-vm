420ed991d7f13339ba3af1608749eda1cd8f7322
Qian Linfeng <thewawar@gmail.com>
2018-08-23 15:41:53 +0800

    refactor: rv32m instruction data structures

--- src/instructions/rv32m.rs
+++ src/instructions/rv32m.rs
@@ -2,59 +2,91 @@ use super::super::machine::Machine;
 use super::super::memory::Memory;
 use super::super::Error;
 use super::utils::{opcode, funct3, funct7, rd, rs1, rs2, update_register};
-use super::{Instruction as GenericInstruction, Instruction::RV32M};
+use super::{
+    Instruction as GenericInstruction,
+    Instruction::RV32M,
+    NextPC,
+    Execute,
+};
 
 #[derive(Debug)]
-pub enum Instruction {
-    // All RV32M extensions are R-type
-    DIV { rd: usize, rs1: usize, rs2: usize },
-    MUL { rd: usize, rs1: usize, rs2: usize },
+pub enum RtypeInstruction {
+    MUL,
+    MULH,
+    MULHSU,
+    MULHU,
+    DIV,
+    DIVU,
+    REM,
+    REMU,
 }
 
-impl Instruction {
-    pub fn execute<M: Memory>(&self, machine: &mut Machine<M>) -> Result<(), Error> {
-        match self {
-            Instruction::DIV { rd, rs1, rs2 } => {
-                let rs1_value: i32 = machine.registers[*rs1] as i32;
-                let rs2_value: i32 = machine.registers[*rs2] as i32;
+pub type Rtype = super::Rtype<RtypeInstruction>;
+
+#[derive(Debug)]
+pub struct Instruction(Rtype);
+
+impl Execute for Rtype {
+    fn execute<M: Memory>(&self, machine: &mut Machine<M>) -> Result<Option<NextPC>, Error> {
+        match &self.inst {
+            RtypeInstruction::MUL => {
+                let rs1_value = machine.registers[self.rs1];
+                let rs2_value = machine.registers[self.rs2];
+                let (value, _) = rs1_value.overflowing_mul(rs2_value);
+                update_register(machine, self.rd, value);
+            },
+            RtypeInstruction::MULH => {},
+            RtypeInstruction::MULHSU => {},
+            RtypeInstruction::MULHU => {},
+            RtypeInstruction::DIV => {
+                let rs1_value: i32 = machine.registers[self.rs1] as i32;
+                let rs2_value: i32 = machine.registers[self.rs2] as i32;
                 let value = if rs2_value == 0 {
                     // This is documented in RISC-V spec, when divided by
                     // 0, RISC-V machine would return -1 in DIV instead of
                     // trapping.
                     -1
                 } else {
-                    let (result, _ ) = rs1_value.overflowing_div(rs2_value);
-                    result
+                    rs1_value.overflowing_div(rs2_value).0
                 };
-                update_register(machine, *rd, value as u32);
+                update_register(machine, self.rd, value as u32);
             },
-            Instruction::MUL { rd, rs1, rs2 } => {
-                let rs1_value = machine.registers[*rs1];
-                let rs2_value = machine.registers[*rs2];
-                let (value, _) = rs1_value.overflowing_mul(rs2_value);
-                update_register(machine, *rd, value);
-            },
+            RtypeInstruction::DIVU => {},
+            RtypeInstruction::REM => {},
+            RtypeInstruction::REMU => {},
         }
-        machine.pc += 4;
+        Ok(None)
+    }
+}
+
+impl Instruction {
+    pub fn execute<M: Memory>(&self, machine: &mut Machine<M>) -> Result<(), Error> {
+        let next_pc = self.0.execute(machine)?;
+        machine.pc = next_pc.unwrap_or(machine.pc + 4);
         Ok(())
     }
 }
 
 pub fn factory(instruction_bits: u32) -> Option<GenericInstruction> {
-    if funct7(instruction_bits) != 0x1 || opcode(instruction_bits) != 0x33 {
-        return None;
-    }
-    match funct3(instruction_bits) {
-        0x0 => Some(RV32M(Instruction::MUL {
+    if opcode(instruction_bits) != 0b_0110011 || funct7(instruction_bits) != 0b_0000001 {
+        None
+    } else {
+        let inst_opt = match funct3(instruction_bits) {
+            0b_000 => Some(RtypeInstruction::MUL),
+            0b_001 => Some(RtypeInstruction::MULH),
+            0b_010 => Some(RtypeInstruction::MULHSU),
+            0b_011 => Some(RtypeInstruction::MULHU),
+            0b_100 => Some(RtypeInstruction::DIV),
+            0b_101 => Some(RtypeInstruction::DIVU),
+            0b_110 => Some(RtypeInstruction::REM),
+            0b_111 => Some(RtypeInstruction::REMU),
+            _ => None,
+        };
+        inst_opt.map(|inst| RV32M(Instruction(Rtype {
             rd: rd(instruction_bits),
             rs1: rs1(instruction_bits),
             rs2: rs2(instruction_bits),
-        })),
-        0x4 => Some(RV32M(Instruction::DIV {
-            rd: rd(instruction_bits),
-            rs1: rs1(instruction_bits),
-            rs2: rs2(instruction_bits),
-        })),
-        _ => None,
+            inst,
+        })))
     }
 }
