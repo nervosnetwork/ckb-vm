c0f0297d441b4c725a3139f012ce9c5f8f815304
Linfeng Qian <thewawar@gmail.com>
2018-08-27 15:26:32 +0800

    test: add more simple instructions test case

--- tests/programs/simple.c
+++ tests/programs/simple.c
@@ -59,5 +59,36 @@ int main() {
   if ((small | small2) != 95) { return 19; }
   if ((small & small2) != 2) { return 20; }
 
+  unsigned int usmall = 5;
+  unsigned int usmall2 = 8;
+  usmall += 82;
+  usmall2 += 2;
+  if ((usmall * usmall2) != 870) { return 21; }
+  if ((usmall / usmall2) != 8) { return 22; }
+  if ((usmall % usmall2) != 7) { return 23; }
+  if ((usmall ^ usmall2) != 93) { return 24; }
+  if ((usmall | usmall2) != 95) { return 25; }
+  if ((usmall & usmall2) != 2) { return 26; }
+
+  int neg_small = 5;
+  int neg_small2 = 8;
+  neg_small -= 499;
+  neg_small2 -= 13;
+  if ((neg_small * neg_small2) != 2470) { return 27; }
+  if ((neg_small / neg_small2) != 98) { return 28; }
+  if ((neg_small % neg_small2) != -4) { return 29; }
+  if ((neg_small ^ neg_small2) != 489) { return 30; }
+  if ((neg_small | neg_small2) != -5) { return 31; }
+  if ((neg_small & neg_small2) != -494) { return 32; }
+  return 0;
+
+  int small4 = 8;
+  small4 += 13;
+  if ((neg_small * small4) != -10374) { return 33; }
+  if ((neg_small / small4) != -23) { return 34; }
+  if ((neg_small % small4) != 10) { return 35; }
+  if ((neg_small ^ small4) != -505) { return 36; }
+  if ((neg_small | small4) != -489) { return 37; }
+  if ((neg_small & small4) != -16) { return 38; }
   return 0;
 }
