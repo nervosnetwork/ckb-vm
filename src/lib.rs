a64283f4db69b4e193991b99ded7e0f0bd048fea
Xuejie Xiao <xxuejie@gmail.com>
2018-09-11 00:37:54 +0000

    fix: disable rustfmt on register names for better visual appearance

--- src/lib.rs
+++ src/lib.rs
@@ -49,10 +49,16 @@ pub const A5: usize = 15;
 pub const A6: usize = 16;
 pub const A7: usize = 17;
 
+#[cfg_attr(rustfmt, rustfmt_skip)]
 pub const REGISTER_ABI_NAMES: [&str; 32] = [
-    "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4",
-    "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4",
-    "t5", "t6",
+    "zero", "ra", "sp", "gp",
+    "tp", "t0", "t1", "t2",
+    "s0", "s1", "a0", "a1",
+    "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3",
+    "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11",
+    "t3", "t4", "t5", "t6",
 ];
 
 #[derive(Debug)]
