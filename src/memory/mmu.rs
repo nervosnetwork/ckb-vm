bfb16708ad6497cc3690283038d5c9187055d90f
Xuejie Xiao <xxuejie@gmail.com>
2018-09-14 02:15:19 +0000

    feat: Speedup MMU with TLB implementation

--- src/memory/mmu.rs
+++ src/memory/mmu.rs
@@ -2,16 +2,13 @@ use super::super::{Error, RISCV_MAX_MEMORY, RISCV_PAGESIZE};
 use super::{Memory, PROT_EXEC, PROT_READ, PROT_WRITE};
 
 use std::cmp::min;
-use std::collections::hash_map::Entry::{Occupied, Vacant};
-use std::collections::HashMap;
 use std::rc::Rc;
 
 pub const MAX_VIRTUAL_MEMORY_ENTRIES: usize = 64;
 
 const MAX_PAGES: usize = RISCV_MAX_MEMORY / RISCV_PAGESIZE;
+const MAX_TLB_ENTRIES: usize = 16;
 const INVALID_PAGE_INDEX: u8 = 0xFF;
-
-type Page = [u8; RISCV_PAGESIZE];
 
 #[inline(always)]
 fn round_page(x: usize) -> usize {
@@ -47,19 +44,41 @@ pub struct VirtualMemoryEntry {
     refcount: usize,
 }
 
+#[derive(Clone, Copy)]
+pub struct TlbEntry {
+    addr: usize,
+    prot: u32,
+    page_data_index: usize,
+}
+
+type Page = [u8; RISCV_PAGESIZE];
+
 /// An MMU implementation with proper protection schemes. Notice this is a correct
 /// soft MMU, not necessarily a fast MMU. For the best performance, we should
 /// leverage native mmap() calls instead to avoid overheads.
 pub struct Mmu {
     vms: Vec<Option<VirtualMemoryEntry>>,
+    // Page table that stores indices of VirtualMemoryEntry for each page. We are
+    // using u8 here to save some memory since we have a hard upper bound of 64
+    // virtual memory entries, which would fit in a u8 perfectly.
     // A real machine would use 2-level page table, but since here we are dealing
     // with 128MB maximum memory, which is just 32K pages, we can just use a single
     // linear array to boost performance.
     page_table: [u8; MAX_PAGES],
-    // key is the start address of the page
-    pages: HashMap<usize, Page>,
+    // Pages that have been requested and instantiated. We are using linear array
+    // here since TLB entry needs to reference individual pages here, and using
+    // a hash map would complicate lifetime rule a lot. We can come back here later
+    // for optimization work if this indeed becomes a bottleneck.
+    // The addresses and data here are stored separately to make it more cache
+    // friendly when we are searching for the page via address.
+    page_addresses: Vec<usize>,
+    page_data: Vec<Page>,
+    // Translation lookaside buffer
+    tlb: [Option<TlbEntry>; MAX_TLB_ENTRIES],
 }
 
+// Generates a new page based on VM mapping, also copy mapped data to the page
+// if exists. Notice every page generated here would be zero-filled first.
 fn handle_page_fault(vm: &VirtualMemoryEntry, addr: usize) -> Result<Page, Error> {
     let mut page = [0; RISCV_PAGESIZE];
     if let Some(ref source) = vm.source {
@@ -79,27 +98,46 @@ impl Mmu {
         Mmu {
             vms: vec![None; MAX_VIRTUAL_MEMORY_ENTRIES],
             page_table: [INVALID_PAGE_INDEX; MAX_PAGES],
-            pages: HashMap::new(),
+            page_addresses: Vec::new(),
+            page_data: Vec::new(),
+            tlb: [None; MAX_TLB_ENTRIES],
         }
+    }
+
+    // Finds requested page index in page_addresses data structure, the same
+    // index could also be used on page_data vec.
+    fn find_page_data_index(&self, addr: usize) -> Option<usize> {
+        self.page_addresses.iter().position(|a| *a == addr)
     }
 
     fn munmap_aligned(&mut self, aligned_addr: usize, aligned_size: usize) -> Result<(), Error> {
         let mut current_addr = aligned_addr;
         while current_addr < aligned_addr + aligned_size {
             let current_page = current_addr / RISCV_PAGESIZE;
-            let index = self.page_table[current_page];
-            if index != INVALID_PAGE_INDEX {
+            let vm_index = self.page_table[current_page];
+            if vm_index != INVALID_PAGE_INDEX {
                 self.page_table[current_page] = INVALID_PAGE_INDEX;
-                self.pages.remove(&current_addr);
-                let mut destroy = false;
-                if let Some(ref mut vm) = self.vms[index as usize] {
-                    vm.refcount -= 1;
-                    if vm.refcount == 0 {
-                        destroy = true;
+                let page_data_index = self.find_page_data_index(current_addr);
+                if let Some(index) = page_data_index {
+                    self.page_addresses.remove(index);
+                    self.page_data.remove(index);
+                }
+                let tlb_index = current_page % MAX_TLB_ENTRIES;
+                let tlb_entry = self.tlb[tlb_index];
+                if let Some(ref tlb) = tlb_entry {
+                    if tlb.addr == current_addr {
+                        self.tlb[tlb_index] = None;
                     }
                 }
-                if destroy {
-                    self.vms[index as usize] = None;
+                let mut destroy_vm_entry = false;
+                if let Some(ref mut vm) = self.vms[vm_index as usize] {
+                    vm.refcount -= 1;
+                    if vm.refcount == 0 {
+                        destroy_vm_entry = true;
+                    }
+                }
+                if destroy_vm_entry {
+                    self.vms[vm_index as usize] = None;
                 }
             }
             current_addr += RISCV_PAGESIZE;
@@ -112,20 +150,41 @@ impl Mmu {
         if page > self.page_table.len() {
             return Err(Error::OutOfBound);
         }
-        let idx = self.page_table[addr / RISCV_PAGESIZE];
-        match self.vms.get(idx as usize) {
+        // Try looking TLB first for a fast path
+        let tlb_index = page % MAX_TLB_ENTRIES;
+        if let Some(entry) = self.tlb[tlb_index] {
+            if entry.addr == addr {
+                if entry.prot & prot == prot {
+                    return Ok(&mut self.page_data[entry.page_data_index]);
+                } else {
+                    return Err(Error::InvalidPermission);
+                }
+            }
+        }
+        // If TLB entry is missing, try looking it from existing instantiated
+        // page data. If it still fails, do a full page fault.
+        let vm_index = self.page_table[addr / RISCV_PAGESIZE];
+        match self.vms.get(vm_index as usize) {
             Some(Some(vm)) => {
                 if vm.prot & prot != prot {
                     return Err(Error::InvalidPermission);
                 }
-                let page = match self.pages.entry(addr) {
-                    Vacant(entry) => {
+                let page_data_index = match self.find_page_data_index(addr) {
+                    Some(index) => index,
+                    None => {
+                        // Do a full page fault here
                         let page = handle_page_fault(vm, addr)?;
-                        entry.insert(page)
+                        self.page_addresses.push(addr);
+                        self.page_data.push(page);
+                        self.page_data.len() - 1
                     }
-                    Occupied(entry) => entry.into_mut(),
                 };
-                Ok(page)
+                self.tlb[tlb_index] = Some(TlbEntry {
+                    addr,
+                    prot: vm.prot,
+                    page_data_index,
+                });
+                Ok(&mut self.page_data[page_data_index])
             }
             _ => Err(Error::OutOfBound),
         }
