9e8d05dae56ed605debdb04bddd2f1b1a824d50c
quake wang <quake.wang@gmail.com>
2019-01-19 18:27:23 +0900

    chore: remove trait found from Syscalls

--- src/syscalls/mod.rs
+++ src/syscalls/mod.rs
@@ -1,8 +1,6 @@
-use super::instructions::Register;
 use super::machine::SupportMachine;
-use super::memory::Memory;
 use super::Error;
-pub trait Syscalls<R: Register, M: Memory> {
+pub trait Syscalls<R, M> {
     fn initialize(&mut self, machine: &mut SupportMachine<REG = R, MEM = M>) -> Result<(), Error>;
     // Returned bool means if the syscall has been processed, if
     // a module returns false, Machine would continue to leverage
