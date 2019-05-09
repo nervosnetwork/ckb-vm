17addb5b63f7348b9899b8c49ad631447bfcabb9
Xuejie Xiao <xxuejie@gmail.com>
2019-05-09 19:50:13 +0800

    feat: shrink VM's memory from 16MB to 4MB

--- definitions/src/lib.rs
+++ definitions/src/lib.rs
@@ -4,7 +4,7 @@ pub mod registers;
 
 pub const RISCV_PAGESIZE: usize = 1 << 12;
 pub const RISCV_GENERAL_REGISTER_NUMBER: usize = 32;
-// 16 MB
-pub const RISCV_MAX_MEMORY: usize = 16 << 20;
-// 2 MB
-pub const DEFAULT_STACK_SIZE: usize = 2 << 20;
+// 4 MB
+pub const RISCV_MAX_MEMORY: usize = 4 << 20;
+// 1 MB
+pub const DEFAULT_STACK_SIZE: usize = 1 << 20;
