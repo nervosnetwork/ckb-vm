bb2e75be2662cba7800450d86860004d54f1555a
Xuejie Xiao <xxuejie@gmail.com>
2019-05-15 11:17:38 +0800

    rename: FLAG_WXORR_BIT => FLAG_WXORX_BIT

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -7,7 +7,7 @@ pub mod sparse;
 pub mod wxorx;
 
 pub use ckb_vm_definitions::memory::{
-    FLAG_EXECUTABLE, FLAG_FREEZED, FLAG_WRITABLE, FLAG_WXORR_BIT,
+    FLAG_EXECUTABLE, FLAG_FREEZED, FLAG_WRITABLE, FLAG_WXORX_BIT,
 };
 
 #[inline(always)]
