d8bdecc2c98cfd55cd447574cf86b1937c3b14f8
Xuejie Xiao <xxuejie@gmail.com>
2019-06-28 06:41:19 +0000

    feat: Add check CI job in case bench fails again

--- benches/vm_benchmark.rs
+++ benches/vm_benchmark.rs
@@ -80,11 +80,6 @@ fn aot_compiling_benchmark(c: &mut Criterion) {
         file.read_to_end(&mut buffer).unwrap();
 
         let buffer = Bytes::from(buffer);
-        let args: Vec<Bytes> = vec!["secp256k1_bench",
-                                      "033f8cf9c4d51a33206a6c1c6b27d2cc5129daa19dbd1fc148d395284f6b26411f",
-                                      "304402203679d909f43f073c7c1dcf8468a485090589079ee834e6eed92fea9b09b06a2402201e46f1075afa18f306715e7db87493e7b7e779569aa13c64ab3d09980b3560a3",
-                                      "foo",
-                                      "bar"].into_iter().map(|a| a.into()).collect();
 
         b.iter(|| {
             AotCompilingMachine::load(&buffer.clone(), None).unwrap().compile().unwrap()
