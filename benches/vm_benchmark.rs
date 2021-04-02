d1525d55a732111f334a87062d2840fb4ea8e40a
mohanson <mohanson@outlook.com>
2021-04-02 09:58:52 +0800

    bench: add mop benchmark

--- benches/vm_benchmark.rs
+++ benches/vm_benchmark.rs
@@ -3,8 +3,12 @@ extern crate criterion;
 
 use bytes::Bytes;
 #[cfg(has_asm)]
-use ckb_vm::machine::{aot::AotCompilingMachine, asm::AsmMachine, VERSION0};
-use ckb_vm::{run, SparseMemory, ISA_IMC};
+use ckb_vm::machine::{
+    aot::AotCompilingMachine,
+    asm::{AsmCoreMachine, AsmMachine},
+    DefaultMachineBuilder, VERSION0, VERSION1,
+};
+use ckb_vm::{run, SparseMemory, ISA_B, ISA_IMC, ISA_MOP};
 use criterion::Criterion;
 use std::fs::File;
 use std::io::Read;
@@ -90,8 +94,33 @@ fn aot_compiling_benchmark(c: &mut Criterion) {
     });
 }
 
+#[cfg(has_asm)]
+fn mop_benchmark(c: &mut Criterion) {
+    c.bench_function("interpret secp256k1_bench via assembly mop", |b| {
+
+        let mut file = File::open("benches/data/secp256k1_bench").unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+
+        let buffer = Bytes::from(buffer);
+        let args: Vec<Bytes> = vec!["secp256k1_bench",
+                                      "033f8cf9c4d51a33206a6c1c6b27d2cc5129daa19dbd1fc148d395284f6b26411f",
+                                      "304402203679d909f43f073c7c1dcf8468a485090589079ee834e6eed92fea9b09b06a2402201e46f1075afa18f306715e7db87493e7b7e779569aa13c64ab3d09980b3560a3",
+                                      "foo",
+                                      "bar"].into_iter().map(|a| a.into()).collect();
+        b.iter(|| {
+            let asm_core = AsmCoreMachine::new(ISA_IMC | ISA_B | ISA_MOP, VERSION1, u64::max_value());
+            let core = DefaultMachineBuilder::<Box<AsmCoreMachine>>::new(asm_core)
+                .build();
+            let mut machine = AsmMachine::new(core, None);
+            machine.load_program(&buffer, &args).unwrap();
+            machine.run().unwrap()
+        });
+    });
+}
+
 #[cfg(not(has_asm))]
-criterion_group!(benches, interpret_benchmark,);
+criterion_group!(benches, interpret_benchmark);
 
 #[cfg(has_asm)]
 criterion_group!(
@@ -99,6 +128,7 @@ criterion_group!(
     interpret_benchmark,
     asm_benchmark,
     aot_benchmark,
-    aot_compiling_benchmark
+    aot_compiling_benchmark,
+    mop_benchmark,
 );
 criterion_main!(benches);
