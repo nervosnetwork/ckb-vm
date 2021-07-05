0f7dd40251027c8748c7be890103d8671050b616
mohanson <mohanson@outlook.com>
2021-07-05 14:00:37 +0800

    Remove unused test binarys

--- tests/test_b_extension.rs
+++ tests/test_b_extension.rs
@@ -1,29 +1,6 @@
 pub mod machine_build;
 
 #[test]
-#[cfg_attr(miri, ignore)] // takes at least 9 hours
-pub fn test_b_extension() {
-    let mut machine = machine_build::int_v1_imcb("tests/programs/b_extension");
-    let ret = machine.run();
-    assert!(ret.is_ok());
-    assert_eq!(ret.unwrap(), 0);
-
-    #[cfg(has_asm)]
-    {
-        let mut machine_asm = machine_build::asm_v1_imcb("tests/programs/b_extension");
-        let ret_asm = machine_asm.run();
-        assert!(ret_asm.is_ok());
-        assert_eq!(ret_asm.unwrap(), 0);
-
-        let code = machine_build::aot_v1_imcb_code("tests/programs/b_extension");
-        let mut machine_aot = machine_build::aot_v1_imcb("tests/programs/b_extension", &code);
-        let ret_aot = machine_aot.run();
-        assert!(ret_aot.is_ok());
-        assert_eq!(ret_aot.unwrap(), 0);
-    }
-}
-
-#[test]
 pub fn test_clzw_bug() {
     let mut machine = machine_build::int_v1_imcb("tests/programs/clzw_bug");
     let ret = machine.run();
@@ -39,51 +16,6 @@ pub fn test_clzw_bug() {
 
         let code = machine_build::aot_v1_imcb_code("tests/programs/clzw_bug");
         let mut machine_aot = machine_build::aot_v1_imcb("tests/programs/clzw_bug", &code);
-        let ret_aot = machine_aot.run();
-        assert!(ret_aot.is_ok());
-        assert_eq!(ret_aot.unwrap(), 0);
-    }
-}
-
-#[test]
-pub fn test_packw_signextend() {
-    let mut machine = machine_build::int_v1_imcb("tests/programs/packw_signextend");
-    let ret = machine.run();
-    assert!(ret.is_ok());
-    assert_eq!(ret.unwrap(), 0);
-
-    #[cfg(has_asm)]
-    {
-        let mut machine_asm = machine_build::asm_v1_imcb("tests/programs/packw_signextend");
-        let ret_asm = machine_asm.run();
-        assert!(ret_asm.is_ok());
-        assert_eq!(ret_asm.unwrap(), 0);
-
-        let code = machine_build::aot_v1_imcb_code("tests/programs/packw_signextend");
-        let mut machine_aot = machine_build::aot_v1_imcb("tests/programs/packw_signextend", &code);
-        let ret_aot = machine_aot.run();
-        assert!(ret_aot.is_ok());
-        assert_eq!(ret_aot.unwrap(), 0);
-    }
-}
-
-#[test]
-pub fn test_single_bit_signextend() {
-    let mut machine = machine_build::int_v1_imcb("tests/programs/single_bit_signextend");
-    let ret = machine.run();
-    assert!(ret.is_ok());
-    assert_eq!(ret.unwrap(), 0);
-
-    #[cfg(has_asm)]
-    {
-        let mut machine_asm = machine_build::asm_v1_imcb("tests/programs/single_bit_signextend");
-        let ret_asm = machine_asm.run();
-        assert!(ret_asm.is_ok());
-        assert_eq!(ret_asm.unwrap(), 0);
-
-        let code = machine_build::aot_v1_imcb_code("tests/programs/single_bit_signextend");
-        let mut machine_aot =
-            machine_build::aot_v1_imcb("tests/programs/single_bit_signextend", &code);
         let ret_aot = machine_aot.run();
         assert!(ret_aot.is_ok());
         assert_eq!(ret_aot.unwrap(), 0);
@@ -131,28 +63,6 @@ pub fn test_rorw_in_end_of_aot_block() {
         let code = machine_build::aot_v1_imcb_code("tests/programs/rorw_in_end_of_aot_block");
         let mut machine_aot =
             machine_build::aot_v1_imcb("tests/programs/rorw_in_end_of_aot_block", &code);
-        let ret_aot = machine_aot.run();
-        assert!(ret_aot.is_ok());
-        assert_eq!(ret_aot.unwrap(), 0);
-    }
-}
-
-#[test]
-pub fn test_fsri_decode_bug() {
-    let mut machine = machine_build::int_v1_imcb("tests/programs/fsri_decode_bug");
-    let ret = machine.run();
-    assert!(ret.is_ok());
-    assert_eq!(ret.unwrap(), 0);
-
-    #[cfg(has_asm)]
-    {
-        let mut machine_asm = machine_build::asm_v1_imcb("tests/programs/fsri_decode_bug");
-        let ret_asm = machine_asm.run();
-        assert!(ret_asm.is_ok());
-        assert_eq!(ret_asm.unwrap(), 0);
-
-        let code = machine_build::aot_v1_imcb_code("tests/programs/fsri_decode_bug");
-        let mut machine_aot = machine_build::aot_v1_imcb("tests/programs/fsri_decode_bug", &code);
         let ret_aot = machine_aot.run();
         assert!(ret_aot.is_ok());
         assert_eq!(ret_aot.unwrap(), 0);
