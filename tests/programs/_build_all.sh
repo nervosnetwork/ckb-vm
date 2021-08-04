9a66faf045bb9f6a0fa6768e70793d236703bdbf
mohanson <mohanson@outlook.com>
2021-08-04 17:13:56 +0800

    Update docker tag

--- tests/programs/_build_all.sh
+++ tests/programs/_build_all.sh
@@ -1,3 +1,3 @@
-BUILDER_DOCKER="nervos/ckb-riscv-gnu-toolchain:bionic-20210727"
+BUILDER_DOCKER="nervos/ckb-riscv-gnu-toolchain:bionic-20210804"
 
 docker run --rm -v `pwd`:/code ${BUILDER_DOCKER} bash -c "cd /code && sh _build_all_native.sh"
