258d1f5538f954fcde018ff5cae7ee716400055f
Xuejie Xiao <xxuejie@gmail.com>
2021-12-14 02:00:09 +0000

    test: Add clmul test case to reveal aarch64 implementation bug

--- tests/test_b_extension.rs
+++ tests/test_b_extension.rs
@@ -102,3 +102,28 @@ pub fn test_pcnt() {
         assert_eq!(ret_aot.unwrap(), 0);
     }
 }
+
+#[test]
+pub fn test_clmul_bug() {
+    let mut machine = machine_build::int_v1_imcb("tests/programs/clmul_bug");
+    let ret = machine.run();
+    assert!(ret.is_ok());
+    assert_eq!(ret.unwrap(), 0);
+
+    #[cfg(has_asm)]
+    {
+        let mut machine_asm = machine_build::asm_v1_imcb("tests/programs/clmul_bug");
+        let ret_asm = machine_asm.run();
+        assert!(ret_asm.is_ok());
+        assert_eq!(ret_asm.unwrap(), 0);
+    }
+
+    #[cfg(has_aot)]
+    {
+        let code = machine_build::aot_v1_imcb_code("tests/programs/clmul_bug");
+        let mut machine_aot = machine_build::aot_v1_imcb("tests/programs/clmul_bug", &code);
+        let ret_aot = machine_aot.run();
+        assert!(ret_aot.is_ok());
+        assert_eq!(ret_aot.unwrap(), 0);
+    }
+}
