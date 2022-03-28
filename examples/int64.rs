592b8d42ec24ea1f93fa716dd2da367f1eda0cf0
mohanson <mohanson@outlook.com>
2022-03-28 11:08:44 +0800

    Add missing vmadd.vv

--- examples/int64.rs
+++ examples/int64.rs
@@ -1,5 +1,5 @@
 use ckb_vm::registers::{A0, A7};
-use ckb_vm::{CoreMachine, Memory, Register, SupportMachine, Syscalls};
+use ckb_vm::{Bytes, CoreMachine, Memory, Register, SupportMachine, Syscalls};
 
 pub struct CustomSyscall {}
 
@@ -39,6 +39,11 @@ impl<Mac: SupportMachine> Syscalls<Mac> for CustomSyscall {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     let code = std::fs::read(&args[1])?.into();
+    let riscv_args: Vec<Bytes> = if args.len() > 2 {
+        (&args[2..]).into_iter().map(|s| s.clone().into()).collect()
+    } else {
+        Vec::new()
+    };
 
     let core_machine = ckb_vm::DefaultCoreMachine::<u64, ckb_vm::SparseMemory<u64>>::new(
         ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_V,
@@ -49,7 +54,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let machine_builder =
         ckb_vm::DefaultMachineBuilder::new(core_machine).instruction_cycle_func(Box::new(|_| 1));
     let mut machine = machine_builder.syscall(Box::new(CustomSyscall {})).build();
-    machine.load_program(&code, &vec!["main".into()]).unwrap();
+    machine.load_program(&code, &riscv_args).unwrap();
 
     let exit = machine.run();
     let cycles = machine.cycles();
