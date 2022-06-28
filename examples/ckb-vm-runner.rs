7e719a9f8099cf692db9935afd1ff409cde69de3
mohanson <mohanson@outlook.com>
2022-06-28 10:15:17 +0800

    Update ckb-vm-runner

--- examples/ckb-vm-runner.rs
+++ examples/ckb-vm-runner.rs
@@ -1,55 +1,6 @@
-use ckb_vm::instructions::{extract_opcode, insts};
+use ckb_vm::instructions::cost_model::instruction_cycles;
 use ckb_vm::registers::{A0, A7};
-use ckb_vm::{Bytes, CoreMachine, Instruction, Memory, Register, SupportMachine, Syscalls};
-
-pub fn instruction_cycles(i: Instruction) -> u64 {
-    match extract_opcode(i) {
-        // IMC
-        insts::OP_JALR => 3,
-        insts::OP_LD => 2,
-        insts::OP_LW => 3,
-        insts::OP_LH => 3,
-        insts::OP_LB => 3,
-        insts::OP_LWU => 3,
-        insts::OP_LHU => 3,
-        insts::OP_LBU => 3,
-        insts::OP_SB => 3,
-        insts::OP_SH => 3,
-        insts::OP_SW => 3,
-        insts::OP_SD => 2,
-        insts::OP_BEQ => 3,
-        insts::OP_BGE => 3,
-        insts::OP_BGEU => 3,
-        insts::OP_BLT => 3,
-        insts::OP_BLTU => 3,
-        insts::OP_BNE => 3,
-        insts::OP_EBREAK => 500,
-        insts::OP_ECALL => 500,
-        insts::OP_JAL => 3,
-        insts::OP_MUL => 5,
-        insts::OP_MULW => 5,
-        insts::OP_MULH => 5,
-        insts::OP_MULHU => 5,
-        insts::OP_MULHSU => 5,
-        insts::OP_DIV => 32,
-        insts::OP_DIVW => 32,
-        insts::OP_DIVU => 32,
-        insts::OP_DIVUW => 32,
-        insts::OP_REM => 32,
-        insts::OP_REMW => 32,
-        insts::OP_REMU => 32,
-        insts::OP_REMUW => 32,
-        // MOP
-        insts::OP_WIDE_MUL => 5,
-        insts::OP_WIDE_MULU => 5,
-        insts::OP_WIDE_MULSU => 5,
-        insts::OP_WIDE_DIV => 32,
-        insts::OP_WIDE_DIVU => 32,
-        insts::OP_FAR_JUMP_REL => 3,
-        insts::OP_FAR_JUMP_ABS => 3,
-        _ => 1,
-    }
-}
+use ckb_vm::{Bytes, CoreMachine, Memory, Register, SupportMachine, Syscalls};
 
 pub struct DebugSyscall {}
 
@@ -91,7 +42,7 @@ fn main_aot(code: Bytes, args: Vec<Bytes>) -> Result<(), Box<dyn std::error::Err
     let mut aot_machine = ckb_vm::machine::aot::AotCompilingMachine::load(
         &code,
         Some(Box::new(instruction_cycles)),
-        ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP,
+        ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP | ckb_vm::ISA_V,
         ckb_vm::machine::VERSION1,
     )?;
     let aot_code = aot_machine.compile()?;
@@ -121,7 +72,7 @@ fn main_aot(code: Bytes, args: Vec<Bytes>) -> Result<(), Box<dyn std::error::Err
 #[cfg(all(has_asm, not(has_aot)))]
 fn main_asm(code: Bytes, args: Vec<Bytes>) -> Result<(), Box<dyn std::error::Error>> {
     let asm_core = ckb_vm::machine::asm::AsmCoreMachine::new(
-        ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP,
+        ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP | ckb_vm::ISA_V,
         ckb_vm::machine::VERSION1,
         u64::MAX,
     );
@@ -145,7 +96,7 @@ fn main_asm(code: Bytes, args: Vec<Bytes>) -> Result<(), Box<dyn std::error::Err
 #[cfg(all(not(has_asm), not(has_aot)))]
 fn main_int(code: Bytes, args: Vec<Bytes>) -> Result<(), Box<dyn std::error::Error>> {
     let core_machine = ckb_vm::DefaultCoreMachine::<u64, ckb_vm::SparseMemory<u64>>::new(
-        ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP,
+        ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP | ckb_vm::ISA_V,
         ckb_vm::machine::VERSION1,
         u64::MAX,
     );
