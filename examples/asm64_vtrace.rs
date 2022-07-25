5d08584485f7e3c984e3807694534017e0cd1c84
Xuejie Xiao <xxuejie@gmail.com>
2022-07-25 03:14:23 +0000

    Time support in vtrace

--- examples/asm64_vtrace.rs
+++ examples/asm64_vtrace.rs
@@ -1,6 +1,8 @@
 use ckb_vm::instructions::cost_model::instruction_cycles;
 use ckb_vm::registers::{A0, A7};
 use ckb_vm::{Bytes, CoreMachine, Memory, Register, SupportMachine, Syscalls};
+use std::time::SystemTime;
+
 pub struct CustomSyscall {}
 
 impl<Mac: SupportMachine> Syscalls<Mac> for CustomSyscall {
@@ -36,6 +38,41 @@ impl<Mac: SupportMachine> Syscalls<Mac> for CustomSyscall {
     }
 }
 
+pub struct TimeSyscall {
+    boot_time: SystemTime,
+}
+
+impl TimeSyscall {
+    fn new() -> Self {
+        Self {
+            boot_time: SystemTime::now(),
+        }
+    }
+}
+
+impl<Mac: SupportMachine> Syscalls<Mac> for TimeSyscall {
+    fn initialize(&mut self, _machine: &mut Mac) -> Result<(), ckb_vm::error::Error> {
+        Ok(())
+    }
+
+    fn ecall(&mut self, machine: &mut Mac) -> Result<bool, ckb_vm::error::Error> {
+        let code = &machine.registers()[A7];
+        // Zlib::crc32("time") % 10000
+        if code.to_i32() != 9285 {
+            return Ok(false);
+        }
+
+        let now = SystemTime::now();
+        let d = now
+            .duration_since(self.boot_time.clone())
+            .expect("clock goes backwards");
+
+        machine.set_register(A0, Mac::REG::from_u64(d.as_nanos() as u64));
+
+        Ok(true)
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     let code = std::fs::read(&args[1])?.into();
@@ -52,6 +89,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let core = ckb_vm::DefaultMachineBuilder::new(asm_core)
         .instruction_cycle_func(Box::new(instruction_cycles))
         .syscall(Box::new(CustomSyscall {}))
+        .syscall(Box::new(TimeSyscall::new()))
         .build();
     let mut machine = ckb_vm::machine::asm::v_trace::VTraceAsmMachine::new(core, None);
 
