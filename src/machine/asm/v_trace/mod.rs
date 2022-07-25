b9526c747c97ce9a6b4e5f6afd57b23485637790
Xuejie Xiao <xxuejie@gmail.com>
2022-07-25 08:54:36 +0000

    Use assembly based fast widening mul

--- src/machine/asm/v_trace/mod.rs
+++ src/machine/asm/v_trace/mod.rs
@@ -605,6 +605,143 @@ fn handle_vmsbc_256<'a>(m: &mut CM<'a>, inst: Instruction) -> Result<(), Error> 
     Ok(())
 }
 
+use std::arch::asm;
+
+#[inline(never)]
+pub fn widening_mul_256(a: &[u8], b: &[u8], dst: &mut [u8], len: usize) {
+    for i in 0..len {
+        // Inspired from https://github.com/cloudflare/bn256/blob/9bd9f73a0273ed2f42707ed13b3e36d38baa2a49/mul_amd64.h#L1
+        unsafe {
+            asm!(
+                "mov rax, [rsi + 0]",
+                "mul qword ptr [rcx + 0]",
+                "mov r8, rax",
+                "mov r9, rdx",
+                "mov rax, [rsi + 0]",
+                "mul qword ptr [rcx + 8]",
+                "add r9, rax",
+                "adc rdx, 0",
+                "mov r10, rdx",
+                "mov rax, [rsi + 0]",
+                "mul qword ptr [rcx + 16]",
+                "add r10, rax",
+                "adc rdx, 0",
+                "mov r11, rdx",
+                "mov rax, [rsi + 0]",
+                "mul qword ptr [rcx + 24]",
+                "add r11, rax",
+                "adc rdx, 0",
+                "mov r12, rdx",
+                "",
+                "mov [rdi + 0], r8",
+                "mov [rdi + 8], r9",
+                "mov [rdi + 16], r10",
+                "mov [rdi + 24], r11",
+                "mov [rdi + 32], r12",
+                "",
+                "mov rax, [rsi + 8]",
+                "mul qword ptr [rcx + 0]",
+                "mov r8, rax",
+                "mov r9, rdx",
+                "mov rax, [rsi + 8]",
+                "mul qword ptr [rcx + 8]",
+                "add r9, rax",
+                "adc rdx, 0",
+                "mov r10, rdx",
+                "mov rax, [rsi + 8]",
+                "mul qword ptr [rcx + 16]",
+                "add r10, rax",
+                "adc rdx, 0",
+                "mov r11, rdx",
+                "mov rax, [rsi + 8]",
+                "mul qword ptr [rcx + 24]",
+                "add r11, rax",
+                "adc rdx, 0",
+                "mov r12, rdx",
+                "",
+                "add r8, [rdi + 8]",
+                "adc r9, [rdi + 16]",
+                "adc r10, [rdi + 24]",
+                "adc r11, [rdi + 32]",
+                "adc r12, 0",
+                "mov [rdi + 8], r8",
+                "mov [rdi + 16], r9",
+                "mov [rdi + 24], r10",
+                "mov [rdi + 32], r11",
+                "mov [rdi + 40], r12",
+                "",
+                "mov rax, [rsi + 16]",
+                "mul qword ptr [rcx + 0]",
+                "mov r8, rax",
+                "mov r9, rdx",
+                "mov rax, [rsi + 16]",
+                "mul qword ptr [rcx + 8]",
+                "add r9, rax",
+                "adc rdx, 0",
+                "mov r10, rdx",
+                "mov rax, [rsi + 16]",
+                "mul qword ptr [rcx + 16]",
+                "add r10, rax",
+                "adc rdx, 0",
+                "mov r11, rdx",
+                "mov rax, [rsi + 16]",
+                "mul qword ptr [rcx + 24]",
+                "add r11, rax",
+                "adc rdx, 0",
+                "mov r12, rdx",
+                "",
+                "add r8, [rdi + 16]",
+                "adc r9, [rdi + 24]",
+                "adc r10, [rdi + 32]",
+                "adc r11, [rdi + 40]",
+                "adc r12, 0",
+                "mov [rdi + 16], r8",
+                "mov [rdi + 24], r9",
+                "mov [rdi + 32], r10",
+                "mov [rdi + 40], r11",
+                "mov [rdi + 48], r12",
+                "",
+                "mov rax, [rsi + 24]",
+                "mul qword ptr [rcx + 0]",
+                "mov r8, rax",
+                "mov r9, rdx",
+                "mov rax, [rsi + 24]",
+                "mul qword ptr [rcx + 8]",
+                "add r9, rax",
+                "adc rdx, 0",
+                "mov r10, rdx",
+                "mov rax, [rsi + 24]",
+                "mul qword ptr [rcx + 16]",
+                "add r10, rax",
+                "adc rdx, 0",
+                "mov r11, rdx",
+                "mov rax, [rsi + 24]",
+                "mul qword ptr [rcx + 24]",
+                "add r11, rax",
+                "adc rdx, 0",
+                "mov r12, rdx",
+                "",
+                "add r8, [rdi + 24]",
+                "adc r9, [rdi + 32]",
+                "adc r10, [rdi + 40]",
+                "adc r11, [rdi + 48]",
+                "adc r12, 0",
+                "mov [rdi + 24], r8",
+                "mov [rdi + 32], r9",
+                "mov [rdi + 40], r10",
+                "mov [rdi + 48], r11",
+                "mov [rdi + 56], r12",
+                in("rsi") a.as_ptr() as usize + i * 32,
+                in("rcx") b.as_ptr() as usize + i * 32,
+                in("rdi") dst.as_ptr() as usize + i * 64,
+                lateout("r12") _,
+                clobber_abi("sysv64"),
+                clobber_abi("win64"),
+            );
+        }
+    }
+}
+
 fn handle_vmmulu_256<'a>(m: &mut CM<'a>, inst: Instruction) -> Result<(), Error> {
     let sew = 256;
     let i = VVtype(inst);
@@ -612,11 +749,18 @@ fn handle_vmmulu_256<'a>(m: &mut CM<'a>, inst: Instruction) -> Result<(), Error>
         if i.vm() == 0 && !m.get_bit(0, j as usize) {
             continue;
         }
-        let b = E256::get(m.element_ref(i.vs2(), sew, j));
-        let a = E256::get(m.element_ref(i.vs1(), sew, j));
-        let (lo, hi) = Eint::widening_mul_u(b, a);
-        lo.put(m.element_mut(i.vd(), sew, j * 2));
-        hi.put(m.element_mut(i.vd(), sew, j * 2 + 1));
+        // Shortcut to create [0u8; 64]
+        let underlying_c = [0u64; 8];
+        let mut c: [u8; 64] = unsafe { transmute(underlying_c) };
+
+        widening_mul_256(
+            m.element_ref(i.vs1(), sew, j),
+            m.element_ref(i.vs2(), sew, j),
+            &mut c,
+            1,
+        );
+
+        m.element_mut(i.vd(), sew * 2, j).copy_from_slice(&c);
     }
     Ok(())
 }
