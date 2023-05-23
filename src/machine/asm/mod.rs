dad4bf03c5f436042b4278a00e45cf7135ca7f59
mohanson <mohanson@outlook.com>
2023-05-23 10:05:18 +0800

    Make Memory::new as a provided method

--- src/machine/asm/mod.rs
+++ src/machine/asm/mod.rs
@@ -193,10 +193,6 @@ fn check_memory_inited(
 impl Memory for Box<AsmCoreMachine> {
     type REG = u64;
 
-    fn new() -> Self {
-        unreachable!()
-    }
-
     fn new_with_memory(_: usize) -> Self {
         unreachable!()
     }
