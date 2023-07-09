2ce859b4d1f94b7b211459ad61e6372e76103c36
Yin Guanhao <sopium@mysterious.site>
2023-07-09 21:14:18 +0800

    Support Vec<BoxedSyscalls>

--- src/syscalls/mod.rs
+++ src/syscalls/mod.rs
@@ -23,3 +23,34 @@ where
         (**self).ecall(machine)
     }
 }
+
+// For better compatibility with existing code that uses Box<dyn Syscalls> or Vec<Box<dyn Syscalls>>.
+
+pub type BoxedSyscalls<Mac> = Box<dyn Syscalls<Mac> + Send + Sync + 'static>;
+
+impl<Mac: SupportMachine> Syscalls<Mac> for BoxedSyscalls<Mac> {
+    fn initialize(&mut self, machine: &mut Mac) -> Result<(), Error> {
+        (**self).initialize(machine)
+    }
+    fn ecall(&mut self, machine: &mut Mac) -> Result<bool, Error> {
+        (**self).ecall(machine)
+    }
+}
+
+impl<Mac: SupportMachine> Syscalls<Mac> for Vec<BoxedSyscalls<Mac>> {
+    fn initialize(&mut self, machine: &mut Mac) -> Result<(), Error> {
+        for s in self {
+            s.initialize(machine)?;
+        }
+        Ok(())
+    }
+    fn ecall(&mut self, machine: &mut Mac) -> Result<bool, Error> {
+        for s in self {
+            let processed = s.ecall(machine)?;
+            if processed {
+                return Ok(processed);
+            }
+        }
+        Ok(false)
+    }
+}
