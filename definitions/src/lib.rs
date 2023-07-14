e2088d2dcd458b61a0c6b750d17b4cca3a812a7d
mohanson <mohanson@outlook.com>
2023-07-14 15:31:41 +0800

    Remove RISCV_MAX_MEMORY_V0

--- definitions/src/lib.rs
+++ definitions/src/lib.rs
@@ -8,7 +8,6 @@ pub const RISCV_PAGESIZE: usize = 1 << RISCV_PAGE_SHIFTS;
 pub const RISCV_GENERAL_REGISTER_NUMBER: usize = 32;
 // 4 MB
 pub const RISCV_MAX_MEMORY: usize = 4 << 20;
-pub const RISCV_MAX_MEMORY_V0: usize = 4 << 20;
 // 1 MB
 pub const DEFAULT_STACK_SIZE: usize = 1 << 20;
 pub const RISCV_PAGES: usize = RISCV_MAX_MEMORY / RISCV_PAGESIZE;
