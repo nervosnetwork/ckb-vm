b1f7e3f5f4dc24cce3f8e38ba8d650e8a4ce9e0d
Mohanson <mohanson@outlook.com>
2023-08-22 11:39:32 +0800

    Use RS3d as temp_regd parameter for CHECK_WRITE macro (#373)
    * Use RS3d as temp_regd for CHECK_WRITE
    
    * Append the negative test case

--- tests/test_a_extension.rs
+++ tests/test_a_extension.rs
@@ -1,4 +1,4 @@
-use ckb_vm::Error;
+use ckb_vm::{CoreMachine, Error, Memory};
 pub mod machine_build;
 
 #[test]
@@ -46,5 +46,25 @@ pub fn test_amo_compare() {
         let ret_asm = machine_asm.run();
         assert!(ret_asm.is_ok());
         assert_eq!(ret_asm.unwrap(), 0);
+    }
+}
+
+#[test]
+pub fn test_amo_check_write() {
+    #[cfg(has_asm)]
+    {
+        let mut machine_asm = machine_build::asm_v2_imacb("tests/programs/amo_check_write");
+        let page_a = 0;
+        let page_b = 17;
+        let flag_a = machine_asm.machine.memory_mut().fetch_flag(page_a).unwrap();
+        assert_eq!(flag_a, 0);
+        let ret_asm = machine_asm.run();
+        assert!(ret_asm.is_ok());
+        assert_eq!(ret_asm.unwrap(), 0);
+        assert_eq!(machine_asm.machine.inner_mut().last_write_page, page_b);
+        let flag_a = machine_asm.machine.memory_mut().fetch_flag(page_a).unwrap();
+        assert_eq!(flag_a, 0);
+        let flag_b = machine_asm.machine.memory_mut().fetch_flag(page_b).unwrap();
+        assert_eq!(flag_b, 4);
     }
 }
