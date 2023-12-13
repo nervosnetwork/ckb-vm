7d73850d7985c68bb5000fcb0a9d45ef6ec9d03e
Mohanson <mohanson@outlook.com>
2023-12-13 17:08:04 +0800

    Backport #389
    * Add a fuzz target to the interpreter (#389)
    
    * Fix compilation issues

--- fuzz/fuzz_targets/interpreter.rs
+++ fuzz/fuzz_targets/interpreter.rs
@@ -1,16 +1,20 @@
 #![no_main]
 use ckb_vm::cost_model::constant_cycles;
-use ckb_vm::machine::asm::{AsmCoreMachine, AsmMachine};
-use ckb_vm::machine::{DefaultMachineBuilder, VERSION2};
+use ckb_vm::machine::{DefaultCoreMachine, DefaultMachineBuilder, VERSION2};
+use ckb_vm::memory::sparse::SparseMemory;
+use ckb_vm::memory::wxorx::WXorXMemory;
 use ckb_vm::{Bytes, ISA_A, ISA_B, ISA_IMC, ISA_MOP};
 use libfuzzer_sys::fuzz_target;
 
 fn run(data: &[u8]) {
-    let asm_core = AsmCoreMachine::new(ISA_IMC | ISA_A | ISA_B | ISA_MOP, VERSION2, 200_000);
-    let core = DefaultMachineBuilder::<Box<AsmCoreMachine>>::new(asm_core)
+    let machine_core = DefaultCoreMachine::<u64, WXorXMemory<SparseMemory<u64>>>::new(
+        ISA_IMC | ISA_A | ISA_B | ISA_MOP,
+        VERSION2,
+        200_000,
+    );
+    let mut machine = DefaultMachineBuilder::new(machine_core)
         .instruction_cycle_func(Box::new(constant_cycles))
         .build();
-    let mut machine = AsmMachine::new(core);
     let program = Bytes::copy_from_slice(data);
     if let Ok(_) = machine.load_program(&program, &[]) {
         let _ = machine.run();
