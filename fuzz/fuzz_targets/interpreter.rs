98787e0a5557cabd5471b962589ffbc9f4f32341
Mohanson <mohanson@outlook.com>
2024-01-30 14:36:53 +0800

    Backport #399
    * Fuzz data will be executed twice and then compared
    
    * Execute 3 times

--- fuzz/fuzz_targets/interpreter.rs
+++ fuzz/fuzz_targets/interpreter.rs
@@ -3,10 +3,10 @@ use ckb_vm::cost_model::constant_cycles;
 use ckb_vm::machine::{DefaultCoreMachine, DefaultMachineBuilder, VERSION2};
 use ckb_vm::memory::sparse::SparseMemory;
 use ckb_vm::memory::wxorx::WXorXMemory;
-use ckb_vm::{Bytes, ISA_A, ISA_B, ISA_IMC, ISA_MOP};
+use ckb_vm::{Bytes, Error, ISA_A, ISA_B, ISA_IMC, ISA_MOP};
 use libfuzzer_sys::fuzz_target;
 
-fn run(data: &[u8]) {
+fn run(data: &[u8]) -> Result<i8, Error> {
     let machine_core = DefaultCoreMachine::<u64, WXorXMemory<SparseMemory<u64>>>::new(
         ISA_IMC | ISA_A | ISA_B | ISA_MOP,
         VERSION2,
@@ -16,11 +16,14 @@ fn run(data: &[u8]) {
         .instruction_cycle_func(Box::new(constant_cycles))
         .build();
     let program = Bytes::copy_from_slice(data);
-    if let Ok(_) = machine.load_program(&program, &[]) {
-        let _ = machine.run();
-    }
+    machine.load_program(&program, &[])?;
+    machine.run()
 }
 
 fuzz_target!(|data: &[u8]| {
-    run(data);
+    let r0 = run(data);
+    let r1 = run(data);
+    let r2 = run(data);
+    assert_eq!(r0, r1);
+    assert_eq!(r1, r2);
 });
