a19d3b8a24d4943839d826d319aa6aa2cdd91bf3
Mohanson <mohanson@outlook.com>
2025-01-27 10:56:28 +0800

    Fix broken fuzz test (#457)

--- fuzz/fuzz_targets/interpreter.rs
+++ fuzz/fuzz_targets/interpreter.rs
@@ -16,7 +16,7 @@ fn run(data: &[u8]) -> Result<i8, Error> {
         .instruction_cycle_func(Box::new(constant_cycles))
         .build();
     let program = Bytes::copy_from_slice(data);
-    machine.load_program(&program, &[])?;
+    machine.load_program(&program, vec![].into_iter())?;
     machine.run()
 }
 
