93e8676b7b93d1d7114ab3523c5a2077074fddbb
mohanson <mohanson@outlook.com>
2025-11-17 15:47:04 +0800

    Use more detailed error information

--- src/machine/trace.rs
+++ src/machine/trace.rs
@@ -217,7 +217,7 @@ impl<Inner: SupportMachine, Decoder: InstDecoder> DefaultMachineRunner
             if self.machine.elp() != 0
                 && extract_opcode(self.traces[slot].instructions[0]) != insts::OP_LPAD
             {
-                return Err(Error::ShadowStackSoftwareCheckException);
+                return Err(Error::ShadowStackNotLpad);
             }
             for i in 0..self.traces[slot].instruction_count {
                 let inst = self.traces[slot].instructions[i as usize];
