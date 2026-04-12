34e907bce6eee0bb8ab96d747de3894b86c9f591
mohanson <mohanson@outlook.com>
2026-04-12 11:34:10 +0800

    Rewrite build.rs

--- build.rs
+++ build.rs
@@ -3,61 +3,50 @@
 // have to keep features always on, and do conditional compilation within the
 // source code
 
+use std::env;
+
 fn main() {
-    use std::env;
-
-    let target_family = env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default();
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
-    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
-    let target = env::var("TARGET").unwrap_or_default();
-    let is_windows = target_family == "windows";
-    let is_msvc = is_windows && (target_env == "msvc");
-    let is_unix = target_family == "unix";
-    let is_x86_64 = target_arch == "x86_64";
-    let is_aarch64 = target_arch == "aarch64";
-    let is_riscv64 = target_arch == "riscv64";
-    let x64_asm = is_x86_64 && (is_windows || is_unix);
-    let aarch64_asm = is_aarch64 && is_unix;
-    // toolchain on sp1 has empty target_family
-    let riscv64_asm = is_riscv64 && (is_unix || target_family.is_empty());
-    let can_enable_asm = x64_asm || aarch64_asm || riscv64_asm;
-
-    if cfg!(feature = "asm") && (!can_enable_asm) {
-        panic!(
-            "Asm feature is not available for target {} on {}!",
-            target_arch, target_family
-        );
+    let support_asm = vec!["aarch64", "riscv64", "x86_64"].contains(&target_arch.as_str());
+    if !support_asm {
+        if cfg!(feature = "asm") {
+            panic!("Asm is not available for {}!", target_arch);
+        }
+        return;
+    }
+    let request_asm = cfg!(feature = "asm") || cfg!(feature = "detect-asm");
+    if !request_asm {
+        return;
     }
 
-    if cfg!(any(feature = "asm", feature = "detect-asm")) && can_enable_asm {
-        println!("cargo:rerun-if-changed=src/machine/asm/execute_x64.S");
-        println!("cargo:rerun-if-changed=src/machine/asm/execute_aarch64.S");
-        println!("cargo:rerun-if-changed=src/machine/asm/execute_riscv64.S");
-        println!("cargo:rerun-if-changed=src/machine/asm/cdefinitions_generated.h");
+    println!("cargo:rustc-cfg=has_asm");
+    println!("cargo:rerun-if-changed=src/machine/asm/execute_x64.S");
+    println!("cargo:rerun-if-changed=src/machine/asm/execute_aarch64.S");
+    println!("cargo:rerun-if-changed=src/machine/asm/execute_riscv64.S");
+    println!("cargo:rerun-if-changed=src/machine/asm/cdefinitions_generated.h");
 
-        let mut build = cc::Build::new();
-
-        if x64_asm {
-            build.file("src/machine/asm/execute_x64.S");
-            if is_msvc {
-                // For now, only an assembly source code is required for CKB-VM, we won't
-                // need to build any C source file here. Hence we can use this simpler solution
-                // to set the default compiler to GCC. We will need to manually trigger the
-                // command to assemble the assembly code file, should any C source file is also
-                // required here.
-                build.compiler("gcc");
-            }
-        } else if aarch64_asm {
-            build.file("src/machine/asm/execute_aarch64.S");
-        } else if riscv64_asm {
-            if target.contains("zkvm") || target.contains("succinct") {
-                build.compiler("riscv64-linux-gnu-gcc");
-            }
-            build.file("src/machine/asm/execute_riscv64.S");
+    match target_arch.as_str() {
+        "aarch64" => {
+            cc::Build::new()
+                .compiler(clang_finder::find())
+                .include("src/machine/asm")
+                .file("src/machine/asm/execute_aarch64.S")
+                .compile("asm");
         }
-
-        build.include("src/machine/asm").compile("asm");
-
-        println!("cargo:rustc-cfg=has_asm")
+        "riscv64" => {
+            cc::Build::new()
+                .compiler(clang_finder::find())
+                .include("src/machine/asm")
+                .file("src/machine/asm/execute_riscv64.S")
+                .compile("asm");
+        }
+        "x86_64" => {
+            cc::Build::new()
+                .compiler(clang_finder::find())
+                .include("src/machine/asm")
+                .file("src/machine/asm/execute_x64.S")
+                .compile("asm");
+        }
+        _ => {}
     }
 }
